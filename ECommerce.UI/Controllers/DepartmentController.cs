4f0106b2b3c825bb67e51adeb1f26ecd3ac74e1f
Sedat <sedattungac12@gmail.com>
2022-03-14 17:52:31 +0300

    .

---
+++ ECommerce.UI/Controllers/DepartmentController.cs
@@ -0,0 +1,23 @@
+﻿using ECommerce.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.UI.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentService _departmentService;
+        public DepartmentController(IDepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+        public IActionResult Index()
+        {
+            var departmentList = _departmentService.GetAllList();
+            return View(departmentList);
+        }
+    }
+}
--- ECommerce.UI/Controllers/HomeController.cs
+++ ECommerce.UI/Controllers/HomeController.cs
@@ -33,5 +33,9 @@ namespace ECommerce.UI.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Test()
+        {
+            return View();
+        }
     }
 }
